08ec2e05476b435f00428901a6a7dbdfede241d3
kana-rus <kanarus786@gmail.com>
2024-09-21 02:54:24 +0900

    refactor: rename `from_interaction_or_args` -> `from_interaction_and_args`

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ use pyinit::{Result, PyInitArgs, PyInit};
 
 fn main() -> Result<()> {
     let args: PyInitArgs = clap::Parser::parse();
-    PyInit::from_interaction_or_args(args)?.run()?;
+    PyInit::from_interaction_and_args(args)?.run()?;
     Ok(())
 }
